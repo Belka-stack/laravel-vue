0436b617f8d1bea97cc395ecf985931c05a225b0
Belka-stack <Belkabakouche@gmail.com>
2025-06-11 18:12:45 +0200

    composants Vue pour formulaire, liste et recherche des recettes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- Application principale encadrée par Vuetify -->
+  <v-app>
+    <v-container>
+      <!-- Titre principal -->
+      <h1 class="text-h4 mb-4">Gestionnaire de Recettes</h1>
+
+      <!-- Barre de recherche pour filtrer les recettes par ingrédient -->
+      <SearchBar @search="handleSearch"/>
+
+      <!-- Formulaire pour ajouter ou modifier une recette-->
+      <RecipeForm
+      :editData="selectedRecipe"
+      @create-recipe="createRecipe"
+      @update-recipe="updateRecipe"
+      @cancel-edit="selectedRecipe = null"
+      />
+
+      <!-- Liste des recettes filtrées -->
+      <RecipeList
+      :recipes="filteredRecipes"/>
+      @edit="selectedRecipe = $event"
+      @delete="deleteRecipe" 
+
+
+    </v-container>
+  </v-app>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Import des composant enfants
+import RecipeForm from './components/RecipeForm.vue';
+import RecipeList from './components/RecipeList.vue';
+import SearchBar from './components/SearchBar.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { SearchBar, RecipeForm, RecipeList},
+  data() {
+    return {
+      recipes: [], // Liste de toutes les recettes
+      selectedRecipe: null, // Recette sélectionnée pour modification
+      filter: '', // Texte utilisé pour filtrer les recettes
+    };
+  },
+  computed: {
+    // Retourne les recettes filtrées selon le champ 'ingrédients'
+    filteredRecipes() {
+      if (!this.filter) return this.recipes;
+      return this.recipes.filter(r =>
+        r.ingredients.toLowerCase().includes(this.filter.toLocaleLowerCase())
+      );
+    },
+  },
+  methods: {
+    // Met à jour le filtre à chaque recherche
+    handleSearch(value) {
+      this.filter = value;
+    },
+    // Les méthodes createRecipe, updateRecipe, deleteReciê seront liées liées à l'API
   }
 }
+
+
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Champ de texte pour filtrer les recettes par ingrédient -->
+    
+    <v-text-field
+    v-model="searchTerm" 
+    label="Rechercher par ingrédient"
+    @input="$emit('search', searchTerm)"
+    append-icon="mdi-magnify"
+    />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+            // Terme de recherche saisi par l'utilisateur
+            searchTerm: '',
+        };
+    },
+};
+
+</script>
